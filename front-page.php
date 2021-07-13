088dbc8ee62c854edad876f037b4c00145b58ab8
Liubov Pellinen <carie@mail.ru>
2021-07-13 15:55:37 +0300

    init

---
+++ front-page.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/iStock-1173740460-2048x1152.jpg')?>);"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large"></h1>
+        <h2 class="headline headline--medium">Create complex quotes 80% faster!</h2>
+        <h3 class="headline headline--small">Digitalize your sales processes with Klaro</h3>
+        <a href="<?php echo get_post_type_archive_link('program');?>" class="btn btn--large btn--blue">Find our more</a>
+      </div>
+    </div>
+
+    <div class="full-width-split group">
+    <!-- The left side --> 
+    <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+                 
+        </div>
+      </div>
+
+    <!-- The right side--> 
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          
+           
+           <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim ut metus vitae gravida. Duis semper sed turpis et efficitur. Nulla facilisi. Maecenas fermentum mattis mi, in fermentum felis tristique at. Nulla aliquam nibh id tempor gravida. In vulputate sapien vel nibh egestas vulputate. Donec elementum dictum sem, sed dapibus leo semper nec. Pellentesque vel facilisis velit, ut hendrerit lorem. Quisque augue tellus, gravida sit amet placerat tempor, scelerisque eu dui. Suspendisse a odio sed sapien laoreet euismod nec quis sapien. Vivamus blandit leo in leo hendrerit auctor.
+          </p>
+
+          <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">Read more</a></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- experiment  -->
+    <div class="full-width-split group">
+    <!-- The left side --> 
+    <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+                 
+        </div>
+      </div>
+
+    <!-- The right side--> 
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          
+           
+           <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim ut metus vitae gravida. Duis semper sed turpis et efficitur. Nulla facilisi. Maecenas fermentum mattis mi, in fermentum felis tristique at. Nulla aliquam nibh id tempor gravida. In vulputate sapien vel nibh egestas vulputate. Donec elementum dictum sem, sed dapibus leo semper nec. Pellentesque vel facilisis velit, ut hendrerit lorem. Quisque augue tellus, gravida sit amet placerat tempor, scelerisque eu dui. Suspendisse a odio sed sapien laoreet euismod nec quis sapien. Vivamus blandit leo in leo hendrerit auctor.
+          </p>
+
+          <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">Read more</a></p>
+        </div>
+      </div>
+
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">XXXXX</h2>
+                 
+        </div>
+      </div>
+    </div>
+
+    <!-- experiment -->
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg')?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Transportation</h2>
+                <p class="t-center">All students have free unlimited bus fare.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg')?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg')?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Food</h2>
+                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
+<?php get_footer();
+?> 
